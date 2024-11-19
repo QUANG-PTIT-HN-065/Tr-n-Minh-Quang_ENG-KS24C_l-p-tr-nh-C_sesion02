384ba346a1c609877a86d47fd9ea0f751167a3a4
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2024-11-19 22:07:01 +0700

    bài 5

---
+++ bai5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // gán giá trị cho chiều dài vào chiều rộng hình chữ nhật
+    int length = 10;
+    int width = 20;
+    // chu vi = (length + width) * 2 
+    int perimeter = (length + width) * 2;
+    // diện tích = length * width
+    int acreage = length * width;
+    return 0;
+}
