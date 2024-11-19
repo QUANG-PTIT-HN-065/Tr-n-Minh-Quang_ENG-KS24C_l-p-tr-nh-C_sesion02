e939f06684a8d90ee427ad3ac2f04803a86e9458
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2024-11-19 22:19:07 +0700

    bài 6

---
+++ bai6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    const float PI = 3.14;
+    // r bán kính hình tròn
+    float r = 5; 
+    // chu vi hình tròn = 2 * PI * r
+    float chu_vi = 2 * PI * r;
+    // diện tích hình tròn = r * r * PI
+    float dien_tich = r * r * PI;
+   return 0;
+}
