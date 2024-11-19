d684d4d3290e70b88257228742d9f62af24b1ef6
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2024-11-19 21:16:23 +0700

    bài 1

---
+++ bai1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    // kieu du lieu so nguyen 
+    int number;
+    // kieu du lieu so thuc , do chinh xac don 
+    float numberA;
+    // kieu du lieu so thuc , do chin xac kep
+    double numberB;
+    // kieu du lieu ki tu
+    char name;
+    return 0; 
+}
