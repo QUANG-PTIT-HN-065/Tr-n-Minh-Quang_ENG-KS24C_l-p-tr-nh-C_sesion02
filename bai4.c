4aecf608750aea142cd4d0ac0c0860c7446d0587
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2024-11-19 21:57:27 +0700

    bài 4

---
+++ bai4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // cạnh hình vuông bằng 10
+    int edge = 10;
+    // chu vi = 10 * 4
+    int chu_vi = edge * 4;
+    // diện tích = cạnh * cạnh 
+    int dien_tich = edge * edge;
+    return 0;
+}
