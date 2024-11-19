19755a7a7fe65f24a640fee5f231320624a33ee3
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2024-11-19 21:40:05 +0700

    bài 3

---
+++ bai3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int num1 = 100; 
+    int num2 = 100;
+    int tong = num1 + num2;
+    // tích 
+    int tich = num1 * num2;
+    printf("tích là %d \n",tich);
+    // thương 
+    int thuong = num1 / num2;
+    printf("thương là %d \n",thuong);
+    // hiệu 
+    int hieu = num2 - num1;
+    printf("hiệu là %d",hieu);
+    return 0;
+}
